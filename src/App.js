44744aab8b0967d086ef82dce8fc5265a6ad556e
Matias Molina <molina.matias.87@gmail.com>
2022-08-15 23:19:27 -0300

    enlazando repo

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+//ENLAZANDO REPOSITORIO
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home/Home"
-import List from "./pages/List/List"
-import Login from "./pages/Login/Login"
-import New from "./pages/New/New"
-import Single from "./pages/Single/Single"
-import NotFound from "./pages/NotFound/NotFound"
-import ProductList from "./pages/Products/Products"
-import { userInputs, productInputs } from "./formSource"
+import Home from "./pages/Home/Home";
+import List from "./pages/List/List";
+import Login from "./pages/Login/Login";
+import New from "./pages/New/New";
+import Single from "./pages/Single/Single";
+import NotFound from "./pages/NotFound/NotFound";
+import ProductList from "./pages/Products/Products";
+import { userInputs, productInputs } from "./formSource";
 
 function App() {
   return (
-
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            
-            <Route index element={ <Home /> }/>
+            <Route index element={<Home />} />
 
-            <Route path="login" element={ <Login /> }/>
-            
+            <Route path="login" element={<Login />} />
+
             <Route path="users">
-              <Route index element={ <List /> } />
-              <Route path=":userId" element={ <Single /> } />
-              <Route path="new" element={ <New inputs={ userInputs } title="Agregar usuario" /> } />
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Agregar usuario" />}
+              />
             </Route>
-            
-            <Route path="products">
-              <Route index element={ <ProductList /> } />
-              <Route path=":productId" element={ <Single /> } />
-              <Route path="new" element={ <New inputs={ productInputs } title="Agregar producto" /> } />
-            </Route>
-            
-            <Route path="*" element={ <NotFound /> } />
 
+            <Route path="products">
+              <Route index element={<ProductList />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={
+                  <New inputs={productInputs} title="Agregar producto" />
+                }
+              />
+            </Route>
+
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </div>
-
   );
 }
 
-export default App
+export default App;
